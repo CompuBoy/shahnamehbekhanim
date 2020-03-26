6ad342047a6a7b684375926635796540770ade4a
qazale <qazal.hosseini@gmail.com>
2020-03-26 19:30:23 +0430

    add bootstrap css to home

--- DotNetCore/Website/Static/scripts/components/home.vue
+++ DotNetCore/Website/Static/scripts/components/home.vue
@@ -1,24 +1,28 @@
 <template>
     <div id="home">
-        <router-link v-bind:to="'/posts'" class="postbox">                                 
+        <router-link v-bind:to="'/posts'" class="row img-responsive postbox">                                 
             <img class="play">
         </router-link>
-        <ul>
-            <li class="box">
+        <div class="row">
+            <div class="col-md-6 box">
                 <router-link v-bind:to="'/about'">                                        
                     <img class="thumbnail aboutthumb">
-                    <strong>داستان ما</strong>
-                    <i>روایت پا گرفتن کانال شاهنامه بخوانیم و سایت</i>
+                    <div class="text-padding">
+                        <strong>داستان ما</strong>
+                        <i>روایت پا گرفتن کانال شاهنامه بخوانیم و سایت</i>
+                    </div>
                 </router-link>
-            </li>
-            <li class="box">
+            </div>
+            <div class="col-md-6 box">
                 <router-link v-bind:to="'/news'">
-                    <img class="thumbnail newsthumb">                                        
-                    <strong>انتشار پادکست</strong>
-                    <i>پادکست شاهنامه بخوانیم منتشر شد</i>
+                    <img class="thumbnail newsthumb">
+                    <div class="text-padding">                                       
+                        <strong>انتشار پادکست</strong>
+                        <i>پادکست شاهنامه بخوانیم منتشر شد</i>
+                    </div>
                 </router-link>
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
 </template>
 
